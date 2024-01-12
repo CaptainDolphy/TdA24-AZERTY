1a91c0654acfc70b890276005922a397cdd32860
Lukas <lukasbajgar2007@gmail.com>
2024-01-12 18:28:36 +0100

    .dockerignore

---
+++ routes/lecturers.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var app = require("../app");
+
+var { v4:uuid } = require('uuid');
+
+const sqlite3 = require('sqlite3').verbose();
+
+router.post('/', async function(req, res, next) {
+  var lecturer = req.body;
+
+  lecturer.uuid = uuid();
+
+  lecturer.tags.forEach(tag => {
+    tag.uuid = uuid();
+  });
+  
+  sqlInsert = `INSERT INTO lecturers(
+    [uuid],
+    [title_before],
+    [first_name],
+    [middle_name],
+    [last_name],
+    [title_after],
+    [picture_url],
+    [location],
+    [claim],
+    [bio],
+    [tags],
+    [price_per_hour],
+    [contact]
+  ) VALUES (
+    ?,?,?,?,?,?,?,?,?,?,?,?,?
+  )`;
+
+  await app.db.run(sqlInsert, [
+    lecturer.uuid,
+    lecturer.title_before,
+    lecturer.first_name,
+    lecturer.middle_name,
+    lecturer.last_name,
+    lecturer.title_after,
+    lecturer.picture_url,
+    lecturer.location,
+    lecturer.claim,
+    lecturer.bio,
+    JSON.stringify(lecturer.tags),
+    lecturer.price_per_hour,
+    JSON.stringify(lecturer.contact)
+  ], (err) => {
+    if(err) console.error(err)
+    else console.log(`Successfully added lecturer ${lecturer.title_before} ${lecturer.first_name} ${lecturer.middle_name} ${lecturer.last_name} ${lecturer.title_after} with uuid: ${lecturer.uuid}`);
+  })
+
+  sql = `SELECT * FROM lecturers`;
+
+  /*
+  app.db.all(sql, [], (err, rows) => {
+    rows.forEach(row => {
+      console.log(row)
+    });
+  }); 
+  */
+
+  res.json(lecturer);
+  res.status(200);
+});
+
+module.exports = router;
