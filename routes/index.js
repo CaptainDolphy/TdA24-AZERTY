627b9d4c58e49891eb1c5ba71b70dd5bedf2af41
captaindolphy <captain.x.dolphy@gmail.com>
2024-02-27 21:27:23 +0100

    fix

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var app = require("../app");
+const { requireAuth } = require("../middleware/authMiddleware.js")
+
+var { v4: uuid } = require('uuid');
+const { indexBookingUuid_get } = require('../controllers/index.js');
+
+const sqlite3 = require('sqlite3').verbose();
+
+
+var indexController = require(path.join(__dirname, '../controllers', 'index'));
+
+router.get('/', indexController.index_get );
+
+router.get('/login',             requireAuth("/"),  indexController.login_get);
+router.get('/booking/:uuid',     indexController.bookingUuid_get)
+router.get('/lecturer/:uuid',                       indexController.lecturerUuid_get);
+
+module.exports = router;
